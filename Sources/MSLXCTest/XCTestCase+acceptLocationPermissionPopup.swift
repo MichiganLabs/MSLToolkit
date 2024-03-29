c6c2a8e1d75d25c5997374aa11d7b747c9ed8af1
Tyler Luce <tjluce@michiganlabs.com>
2024-03-29 11:47:49 -0400

    Update documentation

--- Sources/MSLXCTest/XCTestCase+acceptLocationPermissionPopup.swift
+++ Sources/MSLXCTest/XCTestCase+acceptLocationPermissionPopup.swift
@@ -1,7 +1,7 @@
 import XCTest
 
 extension XCTestCase {
-    /// TODO: Documentation
+    /// If a location permission popup exists, selects the "Allow While Using App" option
     public func acceptLocationPermissionPopup() {
         let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
         let allowBtn = springboard.buttons["Allow While Using App"]
