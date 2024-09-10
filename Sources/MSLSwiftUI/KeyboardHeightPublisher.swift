b14e52286941e70ba723061fa53b7f9d4d703709
John DeLong <jdelong@michiganlabs.com>
2024-09-10 12:24:33 -0400

    Revert format change

--- Sources/MSLSwiftUI/KeyboardHeightPublisher.swift
+++ Sources/MSLSwiftUI/KeyboardHeightPublisher.swift
@@ -4,7 +4,7 @@ import UIKit
 extension Publishers {
     static var keyboardHeight: AnyPublisher<CGFloat, Never> {
         let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
-            .map(\.keyboardHeight)
+            .map { $0.keyboardHeight }
 
         let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
             .map { _ in CGFloat(0) }
