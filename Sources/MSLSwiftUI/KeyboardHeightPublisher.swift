d191ba43fa94162694bffdccec0e9273120ab90b
John DeLong <jdelong@michiganlabs.com>
2024-09-10 12:11:44 -0400

    Add keyboard adaptive view modifier

---
+++ Sources/MSLSwiftUI/KeyboardHeightPublisher.swift
@@ -0,0 +1,21 @@
+import Combine
+import UIKit
+
+extension Publishers {
+    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
+        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
+            .map(\.keyboardHeight)
+
+        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+
+        return MergeMany(willShow, willHide)
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Notification {
+    var keyboardHeight: CGFloat {
+        return (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
+    }
+}
